4d48b3e9d73696b6f2b6ecbd8aa30367d7aa2a09
Nikita <nerdyaus21@gmail.com>
2024-09-10 22:50:03 +0300

    feat [frontend]: added revealing sections

--- script.js
+++ script.js
@@ -193,6 +193,31 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(header)
 
 
+// reveal sections
+// all sections 
+const allSection = document.querySelectorAll('.section')
+console.log(allSection);
+const revealSection = function(entries, observer) {
+  const [entry] = entries
+  console.log(entry);
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden')
+  observer.unobserve(entry.target)
+}
+
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.05
+})
+
+
+allSection.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden')
+})
+
+
 
 
 // TRAVERSING
